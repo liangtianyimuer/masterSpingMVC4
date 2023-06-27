485ae813900eabb98732a92fecd70f6403243e4b
liangtianyimuer <liangtianyimuer@163.com>
2023-06-27 10:25:32 +0800

    test clone from github

---
+++ src/main/java/masterspringmvc4/MasterSpringMvc4Application.java
@@ -0,0 +1,13 @@
+package masterspringmvc4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterSpringMvc4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterSpringMvc4Application.class, args);
+    }
+
+}
---
+++ src/test/java/masterspringmvc4/MasterSpringMvc4ApplicationTests.java
@@ -0,0 +1,13 @@
+package masterspringmvc4;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterSpringMvc4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
